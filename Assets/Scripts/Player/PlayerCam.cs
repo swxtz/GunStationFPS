4d06fbcee2af5ea70fa69f47b5d93550453fa450
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-12 01:14:16 -0300

    feat: player look script

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -20,6 +20,10 @@ public class PlayerCam : MonoBehaviour
         // tira o cursor da tela
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // define a sensibilidade da camera
+        sensX = 50f;
+        sensY = 50f;
     }
 
     // Update is called once per frame
@@ -31,6 +35,13 @@ public class PlayerCam : MonoBehaviour
 
         // isola as mudanças do mouse, suavisando a movimentação
         yRotation += mouseX;
+
         xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //gira a camera e a orientação
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orietation.rotation = Quaternion.Euler(0, yRotation, 0);
+
     }
 }
